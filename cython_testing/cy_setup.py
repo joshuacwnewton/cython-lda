10ffc11b90aa0bb6aa655a7bc8a273dd90c40413
Joshua Newton <joshuacwnewton@gmail.com>
2020-07-14 15:09:48 -0700

    Fix cythonize args in cy_setup.py * Move annotate=True inside cythonize() * Add compiler_directives to address FutureWarning

--- cython_testing/cy_setup.py
+++ cython_testing/cy_setup.py
@@ -4,4 +4,5 @@ from Cython.Build import cythonize
 import Cython.Compiler.Options
 Cython.Compiler.Options.annotate = True
 
-setup(ext_modules=cythonize('cy_lda.pyx'), annotate=True)
+setup(ext_modules=cythonize('cy_lda.pyx', annotate=True,
+                            compiler_directives={'language_level': "3"}))
